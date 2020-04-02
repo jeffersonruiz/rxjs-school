fba2a724d85d2aaf7775cf38d365c43012fdef45
jefferson <jefferson.farid@hotmail.com>
2020-04-02 18:40:06 -0500

    commit clase 57 02/04/2020

--- src/create.js
+++ src/create.js
@@ -1,7 +1,16 @@
 import { displayLog } from './utils';
+import { from } from "rxjs";
 
 export default () => {
     /** start coding */
+
+    const myArray = [1, 2, 3, 4, 5];
+    const myPromise = new Promise(resolve => setTimeout(() => {
+        resolve('Hello Word');
+    }, 2000));
+
+    const observable = from(myPromise);
+    const subsciption = observable.subscribe(val => displayLog(val));
 
     /** end coding */
 }
