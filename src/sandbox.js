2fd393306da1680ed11153107fc5a893a13f1ab6
jefferson <jefferson.farid@hotmail.com>
2020-04-02 19:19:54 -0500

    commit clase 59 02/04/2020

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,8 +1,22 @@
 import { displayLog } from './utils';
+import { fromEvent } from "rxjs";
+import { mapTo, map, filter } from "rxjs/operators";
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
+    const click$ = fromEvent(grid, 'click').pipe(
+        mapTo('CLICK')
+    );
+    const click$_2 = fromEvent(grid, 'click').pipe(
+        map(val => [
+            Math.floor(val.offsetX / 50), 
+            Math.floor(val.offsetY / 50)
+        ]),
+        filter( val => (val[0] + val[1]) % 2 != 0 )
+    );
+    const suscription = click$.subscribe(data => displayLog(data));
+    const suscription_2 = click$_2.subscribe(data_2 => displayLog(data_2));
 
 
     /** end coding */
